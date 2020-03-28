233faa9877dc516e1676228cc4a4e20cfc3a45ce
Joaquin <jngalletto@gmail.com>
2020-03-28 13:34:23 -0300

    add searchbar

--- src/views/AppAppBar.js
+++ src/views/AppAppBar.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import clsx from 'clsx';
+import Button from "@material-ui/core/Button";
+import SearchIcon from '@material-ui/icons/Search';
 import { withStyles } from '@material-ui/core/styles';
+import InputBase from '@material-ui/core/InputBase';
+import { fade } from '@material-ui/core/styles';
 import Link from '@material-ui/core/Link';
 import AppBar from '../components/AppBar';
 import Toolbar, { styles as toolbarStyles } from '../components/Toolbar';
@@ -16,6 +19,8 @@ const styles = (theme) => ({
   },
   left: {
     flex: 1,
+    display: 'flex',
+    justifyContent: 'flex-start',
   },
   leftLinkActive: {
     color: theme.palette.common.white,
@@ -25,13 +30,45 @@ const styles = (theme) => ({
     display: 'flex',
     justifyContent: 'flex-end',
   },
-  rightLink: {
-    fontSize: 16,
-    color: theme.palette.common.white,
-    marginLeft: theme.spacing(3),
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 'auto',
+    },
   },
-  linkSecondary: {
-    color: theme.palette.secondary.main,
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
   },
 });
 
@@ -42,34 +79,32 @@ function AppAppBar(props) {
     <div>
       <AppBar position="fixed">
         <Toolbar className={classes.toolbar}>
-          <div className={classes.left} />
-          <Link
-            variant="h6"
-            underline="none"
-            color="inherit"
-            className={classes.title}
-            href="/premium-themes/onepirate/"
-          >
-            {'CooVIDA'}
-          </Link>
-          <div className={classes.right}>
+          <div className={classes.left}>
             <Link
+              variant="h6"
+              underline="none"
               color="inherit"
-              variant="h6"
-              underline="none"
-              className={classes.rightLink}
-              href="/premium-themes/onepirate/sign-in/"
+              className={classes.title}
+              href="/premium-themes/onepirate/"
             >
-              {'Sign In'}
+              {'CooVIDA'}
             </Link>
-            <Link
-              variant="h6"
-              underline="none"
-              className={clsx(classes.rightLink, classes.linkSecondary)}
-              href="/premium-themes/onepirate/sign-up/"
-            >
-              {'Sign Up'}
-            </Link>
+          </div>
+          <div className={classes.right}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </div>
+            <Button variant="contained">Ayudar</Button>
           </div>
         </Toolbar>
       </AppBar>
